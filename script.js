8c4e8d5ec1dda808051bbba7a9c3720695191e57
Ramon Carrazana Escalona <ramoncarrazanae@gmail.com>
2022-01-10 12:51:43 -0500

    UI terminado

--- script.js
+++ script.js
@@ -13,7 +13,27 @@ function computerPlay(){
     }
 }
 
+let Ccounter = 0;
+let Pcounter = 0;
+
+function declareWinner(){
+    if (Ccounter == 5 || Pcounter ==5){
+        if(Ccounter==5){
+            console.log('Ha ganado la computadora');
+            playerCounter.textContent = "Player: 0";
+            computerCounter.textContent = "Computer: 0"
+        } else {
+            console.log('Has ganado');
+            playerCounter.textContent = "Player: 0";
+            computerCounter.textContent = "Computer: 0"
+        }
+    }
+}
+
 function playRound(playerSelection, computerSelection){
+    console.log("Iniciando Ronda");
+    computerCounter = document.getElementById("computerCounter");
+    playerCounter = document.getElementById("playerCounter");
     if (playerSelection.toUpperCase() == computerSelection.toUpperCase()){
         console.log("Empate")
         return 0
@@ -22,50 +42,47 @@ function playRound(playerSelection, computerSelection){
             case "ROCK":
                 if (computerSelection == "Paper"){
                     console.log("Has perdido. El papel gana a la piedra")
-                    return -1
+                    Ccounter += 1;
+                    computerCounter.textContent = `Computer ${Ccounter}`;
                 } else{
                     console.log("Has ganado. La piedra rompe la tijera")
-                    return 1
+                    Pcounter +=1
+                    playerCounter.textContent = `Player: ${Pcounter}`;
                 }
                 break;
             case "PAPER":
                 if (computerSelection == "Scissors"){
                     console.log("Has perdido. La tijera gana al papel")
-                    return -1
+                    Ccounter += 1;
+                    computerCounter.textContent = `Computer ${Ccounter}`;
                 } else{
                     console.log("Has ganado. El papel gana a la piedra")
-                    return 1
+                    Pcounter +=1
+                    playerCounter.textContent = `Player: ${Pcounter}`;
                 }
                 break;
             case "SCISSORS":
                 if (computerSelection == "Rock"){
                     console.log("Has perdido. La piedra gana a la tijera")
-                    return -1
+                    Ccounter += 1;
+                    computerCounter.textContent = `Computer ${Ccounter}`;
                 } else{
                     console.log("Has ganado. La tijera gana al papel")
-                    return 1
+                    Pcounter +=1
+                    playerCounter.textContent = `Player: ${Pcounter}`;
+                    
                 }
                 break;
         }
     }
+
+    declareWinner();
 }
 
-function game(){
-    counter = 0
-   for (let i = 1; i<=5; i++){
+let buttonRock = document.getElementById("RockButton");
+let buttonScissors = document.getElementById("ScissorsButton");
+let buttonPaper = document.getElementById("PaperButton");
+buttonRock.addEventListener("click",function(){playRound('Rock',computerPlay())});
+buttonScissors.addEventListener("click",function(){playRound('Scissors',computerPlay())});
+buttonPaper.addEventListener("click",function(){playRound('Paper',computerPlay())});
 
-       let playerSelection = prompt("Entre lo que va a usar:");
-       counter += playRound(playerSelection,computerPlay());
-   } 
-
-   console.log("Juego terminado")
-   if (counter > 0){
-       console.log("Has Ganado");
-   } else if(counter==0) {
-       console.log("Estan empatados");
-   } else{
-       console.log("Has Perdido");
-   }
-}
-
-game()
